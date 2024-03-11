2875de9498630b51dc043755287ea832c0bef35a
Micaela Nilsson <micaela.nilsson85@gmail.com>
2024-03-11 18:36:21 +0100

    Added controller, viewmodel,scss.

---
+++ SharedSilicon/Controllers/ContactController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedSilicon.ViewModels;
+
+namespace SharedSilicon.Controllers;
+
+public class ContactController : Controller
+{
+	public IActionResult Index()
+	{
+		ViewData["Title"] = "Contact us";
+
+		var viewModel = new ContactViewModel();
+		return View(viewModel);
+	}
+}
