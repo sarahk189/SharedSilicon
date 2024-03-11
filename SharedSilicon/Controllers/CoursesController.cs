0389d5de751ad02fadc2ccc82d236bae57ddc883
Micaela Nilsson <micaela.nilsson85@gmail.com>
2024-03-11 18:21:50 +0100

    Added controller,model,viewmodel,scss.

---
+++ SharedSilicon/Controllers/CoursesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SharedSilicon.ViewModels;
+
+namespace SharedSilicon.Controllers
+{
+    public class CoursesController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            ViewData["Title"] = "Courses";
+
+            var viewModel = new CoursesViewModel();
+            return View(viewModel);
+        }
+    }
+}
---
+++ SharedSilicon/Models/CoursesModel.cs
@@ -0,0 +1,24 @@
+﻿namespace SharedSilicon.Models;
+
+public class CoursesModel
+{
+
+    public class Course
+    {
+        public string Title { get; set; } = null!;
+        public string Author { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public string? BestBadgeUrl { get; set; }
+        public string BookmarkUrl { get; set; } = null!;
+        public decimal? Price { get; set; }
+        public decimal? RedPrice { get; set; }
+        public decimal? OldPrice { get; set; }
+        public int Hours { get; set; }
+        public int RatingPercentage { get; set; }
+        public string RatingCount { get; set; } = null!;
+
+    }
+
+
+    public List<Course> Courses { get; set; } = [];
+}
