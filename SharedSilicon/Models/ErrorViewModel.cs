60a5521480d641bf3f9c16060d8fba6a05c63da2
Micaela Nilsson <micaela.nilsson85@gmail.com>
2024-03-11 17:01:26 +0100

    .

--- SharedSilicon/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace SharedSilicon.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
