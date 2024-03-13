6640e8172d1ff1f2db0e9c1c27170e309923159c
Hanna <hannagrehn@gmail.com>
2024-03-13 11:59:41 +0100

    lets get it

---
+++ SharedSilicon/Helpers/CheckboxRequired.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace SharedSilicon.Helpers;
+
+public class CheckboxRequired : ValidationAttribute
+{
+    public override bool IsValid(object? value) => value is bool b && b;
+}
+
+
---
+++ SharedSilicon/ViewModels/SignInViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SharedSilicon.Models;
+
+namespace SharedSilicon.ViewModels;
+
+public class SignInViewModel
+{
+
+    public string PageTitle { get; set; } = "Sign in";
+    public SignInModel Form { get; set; } = new SignInModel();
+    public string? ErrorMessage { get; set; }
+}
