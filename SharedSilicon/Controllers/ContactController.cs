46a70aeecfbe8a30c45db8eeae3fb8248136b364
Micaela Nilsson <micaela.nilsson85@gmail.com>
2024-03-14 12:06:03 +0100

    Responsive and validation is done.

--- SharedSilicon/Controllers/ContactController.cs
+++ SharedSilicon/Controllers/ContactController.cs
@@ -5,11 +5,31 @@ namespace SharedSilicon.Controllers;
 
 public class ContactController : Controller
 {
+
 	public IActionResult Index()
 	{
 		ViewData["Title"] = "Contact us";
-
 		var viewModel = new ContactViewModel();
+		return View(viewModel);
+	}
+
+	[HttpPost]
+	public IActionResult Index(ContactViewModel model)
+	{
+		if (!ModelState.IsValid)
+		{
+			return View(model);
+		}
+
+		TempData["MessageSent"] = "Your message has been sent!";
+		ViewData["Title"] = "Contact us";
+
+		// Skapa en ny instans av ContactViewModel
+		var viewModel = new ContactViewModel();
+
+		ModelState.Clear();
+
+		// Returnera den nya instansen till vyn
 		return View(viewModel);
 	}
 }
