dfbf3ef0869447574f96d9ebdf607725f29ae0ac
sarahk189 <143951669+sarahk189@users.noreply.github.com>
2024-03-24 19:38:29 +0100

    status messages to subscribe form

--- SharedSilicon/Controllers/HomeController.cs
+++ SharedSilicon/Controllers/HomeController.cs
@@ -1,26 +1,58 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using SharedSilicon.ViewModels;
+using System.Text.Json.Serialization;
 
 
-namespace SharedSilicon.Controllers
+namespace SharedSilicon.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    //[Route("/")]
+    public IActionResult Index()
     {
-        //[Route("/")]
-        public IActionResult Index()
-        {
-            //var viewModel = new SubscribeViewModel();
-            return View();
-        }
-
-
-        [HttpPost]
-        public IActionResult Subscribe(SubscribeViewModel viewModel)
-        {
-            if (ModelState.IsValid)
-                return RedirectToAction("Success");
-            return View("~/Views/Shared/Sections/_Subscribe.cshtml", viewModel);
-        }
+        //var viewModel = new SubscribeViewModel();
+        return View();
     }
-}
- 
+
+
+    [HttpPost]
+    public /*async <Task>*/IActionResult Subscribe(SubscribeViewModel viewModel)
+    {
+        if (ModelState.IsValid)
+        {
+            return RedirectToAction("Success");
+            return View("~/Views/Shared/Sections/_Subscribe.cshtml", viewModel);
+        //    try
+        //    {
+        //        var content = new StringContent/JsonConverter.SerializeObject(viewModel), Encoding.UTF8, "application/json");
+        //        var response = await _http.PostAsync("https:...", content);
+
+        //        if (response.ISSuccessStatusCode)
+        //        {
+        //            ViewData["Status"] = "Success";
+        //        }
+        //        else if (response.StatusCode == System.Net.HttpStatusCode.Conflict)
+        //        {
+        //            ViewData["Status"] = "AlreadyExists";
+        //        }
+        //        else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+        //        {
+        //            ViewData["Status"] = "Unauhorized";
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        ViewData["Status"] = "ConnectionFailed";
+        //    }
+        //}
+        //else
+        //{
+        //    ViewData["Status"] = "Invalid";
+        //}
+        //return View(viewModel);
+        }
+        return View();
+   
+    }
+} 
