084588dc667f9a5db2075183b96016200eea923b
Micaela Nilsson <micaela.nilsson85@gmail.com>
2024-03-25 11:35:24 +0100

    authcontroller identity

--- SharedSilicon/Controllers/AuthController.cs
+++ SharedSilicon/Controllers/AuthController.cs
@@ -1,10 +1,23 @@
 ﻿using SharedSilicon.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using Infrastructure.Entities;
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Factories;
 
 namespace SharedSilicon.Controllers;
 
 public class AuthController : Controller
 {
+    private readonly UserManager<UserEntity> _userManager;
+
+    public AuthController(UserManager<UserEntity> userManager)
+    {
+		_userManager = userManager;
+	}
+
+
+
     [Route("/signup")]
     [HttpGet]
     public IActionResult SignUp()
@@ -15,10 +28,30 @@ public class AuthController : Controller
 
     [Route("/signup")]
     [HttpPost]
-    public IActionResult SignUp(SignUpViewModel viewModel)
-    {     
-        if (!ModelState.IsValid) 
-            return View(viewModel);
+    public async Task<IActionResult> SignUp(SignUpViewModel viewModel)
+    {
+        if (ModelState.IsValid )
+        {
+            var exists = await _userManager.Users.AnyAsync(x => x.Email == viewModel.Form.Email);
+            if (exists)
+            {
+				ModelState.AddModelError("AlreadyExists", "Email already exists");
+                ViewData["ErrorMessage"] = "User with the same email address already exists";
+				return View(viewModel);
+			}
+
+            var userEntity = UserFactory.CreateUser(viewModel.Form.FirstName, viewModel.Form.LastName, viewModel.Form.Email);
+
+
+            var result = await _userManager.CreateAsync(userEntity, viewModel.Form.Password);
+            if (result.Succeeded)
+            { 
+                return RedirectToAction("SignIn", "Auth"); 
+            }
+            
+        }
+        //if (!ModelState.IsValid) 
+        //    return View(viewModel);
 
         return RedirectToAction("SignIn", "Auth");
     }
