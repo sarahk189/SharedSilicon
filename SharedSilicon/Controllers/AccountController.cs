7f379a5cbb852650c74cc6a02a49b81c5617798c
sarahk189 <143951669+sarahk189@users.noreply.github.com>
2024-03-25 20:07:48 +0100

    services in controllers

--- SharedSilicon/Controllers/AccountController.cs
+++ SharedSilicon/Controllers/AccountController.cs
@@ -2,9 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
 using Infrastructure.Entities;
+using Microsoft.AspNetCore.Authorization;
+using SharedSilicon.Models;
 
 namespace SharedSilicon.Controllers;
 
+[Authorize]
 public class AccountController : Controller
 {
     private readonly UserManager<UserEntity> _userManager;
@@ -14,17 +17,88 @@ public class AccountController : Controller
         _userManager = userManager;
     }
 
-    [Route("/account")]
-    public IActionResult Details()
+    #region Details
+    [Route("/account/details")]
+    public async Task <IActionResult> Details()
     {
-        var viewModel = new AccountDetailsViewModel();
+        var claims = HttpContext.User.Identities.FirstOrDefault();
+        var viewModel = await PopulateViewModelAsync();
         //viewModel.BasicInfo = _accountService.GetBasicInfo();
         //viewModel.AddressInfo = _accountService.GetAddressInfo();
 
         return View(viewModel);
     }
 
-    [Route("/security")]
+    public async Task<AccountDetailsViewModel> PopulateViewModelAsync()
+    {
+        var user = await _userManager.GetUserAsync(User);
+        
+        try
+        {
+            if (user != null)
+            {
+                var address = user.Address;
+                var viewModel = new AccountDetailsViewModel()
+                {
+                    BasicInfo = new AccountDetailsBasicInfoModel
+                    {
+                        ProfileImage = user.ProfileImage,
+                        FirstName = user.FirstName,
+                        LastName = user.LastName,
+                        Email = user.Email!,
+                        Phone = user.PhoneNumber!,
+                        Biography = user.Biography
+                    },
+                    AddressInfo = new AccountDetailsAddressInfoModel
+                    {
+                        Addressline_1 = address.AddressLine1,
+                        Addressline_2 = address.AddressLine2!,
+                        PostalCode = address.PostalCode, 
+                        City = address.City
+                    }
+                };
+                return viewModel;
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+        return null!;
+    }
+
+    [HttpGet]
+    public IActionResult BasicInfo()
+    {
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult BasicInfo(AccountDetailsViewModel viewModel)
+    {
+        //_accountService.SaveBasicInfo(viewModel.BasicInfo);
+        return RedirectToAction(nameof(Details), viewModel);
+    }
+
+    [HttpGet]
+
+    public IActionResult AddressInfo()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult AddressInfo(AccountDetailsViewModel viewModel)
+    {
+        //_accountService.SaveAddressInfo(viewModel.AddressInfo);
+        return RedirectToAction(nameof(Details), viewModel);
+    }
+
+    #endregion
+
+    #region Security
+    [Route("/account/security")]
     public /*async Task<IActionResult>*/ IActionResult Security(SecurityViewModel viewModel)
     {
 
@@ -55,40 +129,15 @@ public class AccountController : Controller
 
         return View(viewModel);
     }
+    #endregion
 
-
-    [Route("/mycourses")]
+    #region MyCourses
+    [Route("/account/mycourses")]
     public IActionResult SavedCourses(SavedCoursesViewModel viewModel)
     {
         return View (viewModel);
     }
+    #endregion
 
-    [HttpGet]
-    public IActionResult BasicInfo()
-    {
-
-        return View();
-    }
-
-    [HttpPost]
-    public IActionResult BasicInfo(AccountDetailsViewModel viewModel)
-    {
-        //_accountService.SaveBasicInfo(viewModel.BasicInfo);
-        return RedirectToAction(nameof(Details), viewModel);
-    }
-
-    [HttpGet]
-
-    public IActionResult AddressInfo()
-    {
-        return View();
-    }
-
-    [HttpPost]
-    public IActionResult AddressInfo(AccountDetailsViewModel viewModel)
-    {
-        //_accountService.SaveAddressInfo(viewModel.AddressInfo);
-        return RedirectToAction(nameof(Details), viewModel);
-    }
 
 }
--- SharedSilicon/Controllers/HomeController.cs
+++ SharedSilicon/Controllers/HomeController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using SharedSilicon.ViewModels;
 using System.Text.Json.Serialization;
+using System.Text.Unicode;
 
 
 namespace SharedSilicon.Controllers;
@@ -17,42 +18,38 @@ public class HomeController : Controller
 
 
     [HttpPost]
-    public /*async <Task>*/IActionResult Subscribe(SubscribeViewModel viewModel)
+    public async Task <IActionResult> Subscribe(SubscribeViewModel viewModel)
     {
         if (ModelState.IsValid)
         {
-            return RedirectToAction("Success");
-            return View("~/Views/Shared/Sections/_Subscribe.cshtml", viewModel);
-        //    try
-        //    {
-        //        var content = new StringContent/JsonConverter.SerializeObject(viewModel), Encoding.UTF8, "application/json");
-        //        var response = await _http.PostAsync("https:...", content);
+            //    try
+            //    {
+            //        var content = new StringContent/JsonConverter.SerializeObject(viewModel), Encoding.Utf8, "application/json");
+            //        var response = await _http.PostAsync("https:...", content);
 
-        //        if (response.ISSuccessStatusCode)
-        //        {
-        //            ViewData["Status"] = "Success";
-        //        }
-        //        else if (response.StatusCode == System.Net.HttpStatusCode.Conflict)
-        //        {
-        //            ViewData["Status"] = "AlreadyExists";
-        //        }
-        //        else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-        //        {
-        //            ViewData["Status"] = "Unauhorized";
-        //        }
-        //    }
-        //    catch
-        //    {
-        //        ViewData["Status"] = "ConnectionFailed";
-        //    }
-        //}
-        //else
-        //{
-        //    ViewData["Status"] = "Invalid";
-        //}
-        //return View(viewModel);
+            //        if (response.ISSuccessStatusCode)
+            //        {
+            //            ViewData["Status"] = "Success";
+            //        }
+            //        else if (response.StatusCode == System.Net.HttpStatusCode.Conflict)
+            //        {
+            //            ViewData["Status"] = "AlreadyExists";
+            //        }
+            //        else if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
+            //        {
+            //            ViewData["Status"] = "Unauhorized";
+            //        }
+            //    }
+            //    catch
+            //    {
+            //        ViewData["Status"] = "ConnectionFailed";
+            //    }
+            //}
+            //else
+            //{
+            //    ViewData["Status"] = "Invalid";
         }
-        return View();
-   
+        return View(viewModel);
     }
-} 
+
+}
