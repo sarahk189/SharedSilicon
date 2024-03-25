5c7f1b82ab5b40c5c67adda966e88214dfb032a1
Micaela Nilsson <micaela.nilsson85@gmail.com>
2024-03-25 09:42:30 +0100

    User and AddressEntity done.

--- Infrastructure/Entities/UserEntity.cs
+++ Infrastructure/Entities/UserEntity.cs
@@ -1,20 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 
 namespace Infrastructure.Entities;
 
-public class UserEntity
+public class UserEntity : IdentityUser
 {
-	[Key]
-	public int Id { get; set; }
+
+	[ProtectedPersonalData]
 	public string FirstName { get; set; } = null!;
 
+	[ProtectedPersonalData]
 	public string LastName { get; set; } = null!;
-	public string Email { get; set; } = null!;
-	public string Password { get; set; } = null!;
-	public string SecurityKey { get; set; } = null!;
-	public string? Phone { get; set; }
+
+	[ProtectedPersonalData]
 	public string? Biography { get; set; }
+
+	[ProtectedPersonalData]
 	public string? ProfileImage { get; set; }
 
 	public DateTime? Created { get; set; }
