29223a2dccc467e06c0574a32650c4510c397c49
sarahk189 <143951669+sarahk189@users.noreply.github.com>
2024-03-25 09:48:06 +0100

    DbSet Entities

--- Infrastructure/Contexts/DataContext.cs
+++ Infrastructure/Contexts/DataContext.cs
@@ -6,11 +6,11 @@ namespace Infrastructure.Contexts;
 
 public class DataContext(DbContextOptions<DataContext> options) : DbContext(options)
 {
-
 	public DbSet<UserEntity> Users { get; set; } = null!;
 	public DbSet<AddressEntity> Addresses { get; set; } = null!;
-
 	public DbSet<SubscribeEntity> Subscribe { get; set; } = null!;
-
+	public DbSet<CourseEntity> Courses { get; set; } = null!;
+	public DbSet<CourseDetailsEntity> CoursesDetails { get; set;} = null!;
+	public DbSet<CourseAuthorEntity> CoursesAuthor { get; set;} = null!;
 
 }
