fa978d623d8d02460cfe39ac41e5c69831ed38f1
Hanna <hannagrehn@gmail.com>
2024-03-28 14:38:22 +0100

    lets

---
+++ SharedSilicon/Controllers/SiteSettings.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SharedSilicon.Controllers;
+
+public class SiteSettings : Controller
+{
+    public IActionResult ChangeTheme(string mode)
+    {
+        var option = new CookieOptions
+        {
+            Expires = DateTime.Now.AddDays(30),
+        };
+        Response.Cookies.Append("ThemeMode", mode, option);
+        return Ok();
+    }
+}
