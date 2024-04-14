7589c9a381d22185018b743df44a94f3bdc491f0
sarahk189 <143951669+sarahk189@users.noreply.github.com>
2024-04-14 20:20:23 +0200

    courses reverting

--- SharedSilicon/Controllers/CoursesController.cs
+++ SharedSilicon/Controllers/CoursesController.cs
@@ -12,40 +12,17 @@ using static SharedSilicon.Models.CoursesModel;
 
 namespace SharedSilicon.Controllers;
 
-[Authorize]
 public class CoursesController(CategoryService categoryService, CourseService courseService, IConfiguration configuration, HttpClient http) : Controller
 {
-	private readonly CategoryService _categoryService = categoryService;
-	private readonly CourseService _courseService = courseService;
+    private readonly CategoryService _categoryService = categoryService;
+    private readonly CourseService _courseService = courseService;
     private readonly IConfiguration _configuration = configuration;
     private readonly HttpClient _http = http;
 
-	public async Task<IActionResult> Index()
-	{
-		if (HttpContext.Request.Cookies.TryGetValue("AccessToken", out var token))
-		{
-			_http.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-			var response = await _http.GetAsync($"https://localhost:7152/api/Courses?key={_configuration["ApiKey:Secret"]}");
-			if (response.IsSuccessStatusCode)
-			{
-				var data = await response.Content.ReadAsStringAsync();
-				var courses = JsonConvert.DeserializeObject<IEnumerable<CourseDto>>(data);
-
-				var categories = await _categoryService.GetCategoriesAsync();
-
-				var viewModel = new CourseIndexViewModel
-				{
-					Categories = categories,
-					Courses = courses
-				};
-
-				return View(viewModel);
-			}
-		}
-		return View();
-	}
-	//      var categories = await _categoryService.GetCategoriesAsync();
-	//var courses = await _courseService.GetCoursesAsync();
+    public async Task<IActionResult> Index()
+    {
+        var categories = await _categoryService.GetCategoriesAsync();
+        var courses = await _courseService.GetCoursesAsync();
 
         var viewModel = new CourseIndexViewModel
         {
@@ -55,13 +32,13 @@ public class CoursesController(CategoryService categoryService, CourseService co
 
         return View(viewModel);
     }
+	
 
     [Route("Courses/Details/{id}")]
     [HttpGet("Details/{id}")]
     public async Task<IActionResult> CourseDetails(int id)
     {
         using var http = new HttpClient();
-        var apiKey = "Yzg3OGM2MjAtZGRjYi00YzQ2LWI4M2YtY2M2Yzk2MmQyZWNh";
         var response = await http.GetAsync($"https://localhost:7152/api/courses/{id}?key={_configuration["ApiKey:Secret"]}");
         var json = await response.Content.ReadAsStringAsync();
         var courseDto = JsonConvert.DeserializeObject<Infrastructure.Dtos.CourseDto>(json);
