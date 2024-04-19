8e3c63acf79a3113dde4dcca437351f7e75278ee
Hanna <hannagrehn@gmail.com>
2024-04-19 18:22:34 +0200

    KLAURRAA

--- WebApi/Controllers/ContactController.cs
+++ WebApi/Controllers/ContactController.cs
@@ -48,37 +48,3 @@ public class ContactController(DataContext context) : ControllerBase
     }
 }
 
-
-    //public class ContactController(DataContext context) : ControllerBase
-    //{
-    //    private readonly DataContext _context = context;
-
-    //    [HttpPost("send")]
-    //    //[UseApiKey]
-    //    public async Task<IActionResult> Send([FromBody] ContactDto input)
-    //    {
-
-    //        if (ModelState.IsValid)
-    //        {
-    //            if (string.IsNullOrEmpty(input.FullName) || string.IsNullOrEmpty(input.Email) || string.IsNullOrEmpty(input.Message))
-    //            {
-    //                return BadRequest("All fields are required.");
-    //            }
-
-    //            var contactRequest = new ContactRequestEntity
-    //            {
-    //                FullName = input.FullName,
-    //                EmailAddress = input.Email,
-    //                Message = input.Message
-    //            };
-
-    //            _context.ContactRequests.Add(contactRequest);
-
-    //            await _context.SaveChangesAsync();
-    //            return Ok();
-    //        }
-    //        return BadRequest(ModelState);
-
-    //    }
-
-    //}
